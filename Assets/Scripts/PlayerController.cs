186fcbb9eb7871c78a854fb5cf3b2a1900d9ecd3
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2023-10-17 15:47:49 -0700

    Implemented Speed Limits and Jumping
    Use raycasting to detect if the ball is on the floor before jumping. Detect if the ball is going too fast and clamp the speed if it is. Also, added a texture to the ground.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private int count;
     private float movementX;
+    private float movementZ;
     private float movementY;
     public float speed = 0;
+    public float jumpStrength = 0;
+    public float maxSpeed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
@@ -26,9 +29,24 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        Vector3 movement = new Vector3(movementVector.x, 0.0f, movementVector.y);
 
         movementX = movementVector.x;
-        movementY = movementVector.y;
+        movementZ = movementVector.y;
+    }
+
+    void OnJump() 
+    {
+        RaycastHit hitData;
+
+        if(Physics.Raycast(rb.position, Vector3.down, out hitData, 1))
+        {
+            if(hitData.transform.CompareTag("Ground") && Mathf.Abs(Vector3.Dot(hitData.normal, Vector3.down)) > 0.707)
+            {
+                movementY = 1;
+                rb.AddForce(Vector3.up * jumpStrength);
+            }
+        }
     }
 
     void SetCountText()
@@ -41,10 +59,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(movementX, movementY, movementZ);
         rb.AddForce(movement * speed);
+    }*/
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, movementY, movementZ);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+
+        Vector3 velocity = rb.velocity;
+
+        Vector2 horizontalVelocity = new Vector2(velocity.x, velocity.z);
+
+        if(horizontalVelocity.magnitude > maxSpeed)
+        {
+            Vector2 clampedVelocity = horizontalVelocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(clampedVelocity.x, velocity.y, clampedVelocity.y);
+        }
+
+        Debug.Log("The current speed is " + horizontalVelocity.magnitude.ToString());
+
+
     }
 
     void OnTriggerEnter(Collider other)
